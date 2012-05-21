28e47a9cc3d1c7bfce7e8e1120be211149484d0c
iamwyza <iamwyza@gmail.com>
2012-05-21 15:58:35 -0400

    Updated to 3.2

--- Portlet.CUS.SimpleQuery/FWK_VersionInfo.sql
+++ Portlet.CUS.SimpleQuery/FWK_VersionInfo.sql
@@ -1,8 +1,8 @@
 begin tran 
 use ICS_NET; 
 Declare @ApplicationName varchar(255) = 'CUS Simple Query Portlet'; 
-Declare @VersionNumber varchar(255) = '3.1.2'; 
-Declare @VersionDate DateTime = '2012-04-14 11:07:00'; 
+Declare @VersionNumber varchar(255) = '3.2.0'; 
+Declare @VersionDate DateTime = '2012-05-8 9:49:00'; 
 if exists (select * from [FWK_VersionInfo] where [ApplicationName] = @ApplicationName) 
 begin 
    update [FWK_VersionInfo] set VersionNumber = @VersionNumber, VersionDate = @VersionDate 
